a1ecde51190d222e50d8347498b624181643a6e8
弘树 <tiehang.lth@alibaba-inc.com>
2014-11-25 09:26:14 +0800

    sync daily version 1.0.0

--- 1.0.0/build/index.js
+++ 1.0.0/build/index.js
@@ -1,5 +1,5 @@
 /*
-Tue Nov 04 2014 13:27:34 GMT+0800 (CST)
+Tue Nov 25 2014 08:42:29 GMT+0800 (CST)
 combined files by KMD:
 
 index.js
@@ -11,12 +11,43 @@ var Base = require('base');
 
 var UnfoldUp = Base.extend({
     initializer:function(){
+        var height = this.get('closeable') ? this.get('min') : this.get('max');
+        this.get('container').css({
+            "overflow": "hidden",
+            "height": height
+        });
+        this._bind();
+    },
+    _bind:function(){
         var self = this;
-        var $target = self.get('$target');
+        var close = this.get('closeable');
+        this.get('action').on('click',function(){
+            var height = close ? self.get('max') : self.get('min');
+            self._animate(height);
+            close = !close;
+        });
+    },
+    _animate:function(height){
+        var self = this;
+        this.get('container').animate({
+            height: height
+        },0.4,undefined,function(){
+            var eventName = height == self.get('min') ? 'close' : 'open';
+            self.fire(eventName);
+        });
     }
 },{
     ATTRS:{
-        $target:{
+        min: 0,
+        max: 300,
+        closeable: true,
+        action:{
+            value:'',
+            getter:function(v){
+                return $(v);
+            }
+        },
+        container:{
             value:'',
             getter:function(v){
                 return $(v);
